e70dee7fd4400908a85250965f4f7eff66efb32f
Akshay Gore <akshaygore2301@gmail.com>
2022-03-22 12:01:55 +0530

    First commit

---
+++ src/components/Pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserResults from '../user/UserResults'
+import UserSearch from '../user/UserSearch'
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  )
+}
+
+export default Home
